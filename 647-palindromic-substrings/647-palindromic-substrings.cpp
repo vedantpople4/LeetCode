98220aa45e08e81f0ca7a200400c6d993d3aaa28
Vedant pople <vedantpople4@gmail.com>
2022-05-22 08:11:13 +0530

    Time: 10 ms (62.34%), Space: 6.3 MB (82.67%) - LeetHub

---
+++ 647-palindromic-substrings/647-palindromic-substrings.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int countSubstrings(string s) {
+        int ans = 0;
+        for(int i=0;i<s.size();i++){
+            int left = i, right = i;
+            while(left>=0 && right < s.size() && s[left]==s[right]){
+                ans++;
+                left--;
+                right++;
+                }
+            left = i, right = i+1;
+            while(left>=0 && right < s.size() &&s[left]==s[right]){
+                ans++;
+                left--;
+                right++;
+            }
+        }
+        return ans;
+    }
+};
