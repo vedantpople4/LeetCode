26961b6073d44669d42a5060379e3eb1c26688c8
Vedant Pople <vedantpople4@gmail.com>
2022-06-13 16:27:06 +0530

    Time: 6 ms (80.20%), Space: 8.6 MB (70.75%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     int minimumTotal(vector<vector<int>>& arr) {
-       // int sum = arr[0][0];
         for(int i=arr.size()-2;i>=0;i--){
             for(int j=0;j<=i;j++){
                 arr[i][j] = min(arr[i+1][j],arr[i+1][j+1]) + arr[i][j];          
