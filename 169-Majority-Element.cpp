5c6c15e22b2083e9e86bb6a54a682ae52950312f
Vedant Pople <vedantpople4@gmail.com>
2024-08-20 22:14:42 -0700

    [169] [Time Beats: 5.29%] [Memory Beats: 99.90%] - LeetPush

---
+++ 169-Majority-Element.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int e1 = nums[0], e2 = 0, c1 = 0, c2 = 0;
+        for(int i=0;i<nums.size();i++){
+            
+            
+        }
+    }
+};
